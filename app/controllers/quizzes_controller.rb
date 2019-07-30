ea8c1b0a3b125694ff1620894d561a4d39a13661
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-07-30 15:23:44 -0700

    still working on wiring up quiz form

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,5 @@
 class QuizzesController < ApplicationController
-
+    
     def index
         @quizzes = Quiz.all 
     end
@@ -7,10 +7,12 @@ class QuizzesController < ApplicationController
     def new
         #@questions = Qustion.all 
         #@answer = Answer.all 
-        @quiz = Quiz.new 
+        #@quiz = Quiz.find(params[:id])
     end
 
     def create 
+        @quiz = Quiz.find(params[:id])
+        redirect_to new_quiz_path(@quiz)
     end
 
 
