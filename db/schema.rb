88d498facbe1e3d31bd3ad1a2a608985f76bfd6b
aminamos <26092352+aminamos@users.noreply.github.com>
2019-07-30 14:31:46 -0700

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_171204) do
+ActiveRecord::Schema.define(version: 2019_07_30_172126) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quiz_id"
   end
 
   create_table "feedbacks", force: :cascade do |t|
@@ -41,7 +40,6 @@ ActiveRecord::Schema.define(version: 2019_07_30_171204) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quiz_id"
-    t.integer "category_id"
   end
 
   create_table "quizzes", force: :cascade do |t|
@@ -56,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_07_30_171204) do
     t.datetime "updated_at", null: false
     t.integer "person_id"
     t.integer "quiz_id"
-    t.string "category_id"
   end
 
 end
