5a816cb7c9129bcf17635b015809ae38534c12a5
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-07-31 09:17:51 -0700

    just need to pull

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,17 +1,19 @@
 class QuizzesController < ApplicationController
     
     def index
-        @quizzes = Quiz.all 
+        puts Quiz.count
+        @quizzes = Quiz.all
     end
 
     def new
         #@questions = Qustion.all 
         #@answer = Answer.all 
-        #@quiz = Quiz.find(params[:id])
+        #@quiz = Quiz.new
+        @quiz = Quiz.create(description: "vacation").description
     end
 
     def create 
-        @quiz = Quiz.find(params[:id])
+        #@quiz = Quiz.find(params[:id])
         redirect_to new_quiz_path(@quiz)
     end
 
